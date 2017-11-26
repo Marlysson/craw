7e448d8bf8b30ffec3e314cb6d6b0de7c52cd4ff
Marlysson <marlysson5@gmail.com>
2017-11-25 21:53:57 -0300

    Creating function to normalize texts to capitalize each world that your len() is bigger that 3.

--- core/resources/crawlers/utils.py
+++ core/resources/crawlers/utils.py
@@ -1,29 +1,53 @@
 from selenium import webdriver
+import requests
+import xml.etree.ElementTree as ET
 from os import path , getcwd
 import csv
 
 CRAWLER_DIR = path.dirname(getcwd())
+DATA_DIR = path.join(CRAWLER_DIR,"data")
+
+URL_RESOURCES = {
+    "world_cup": "http://www.portaltransparencia.gov.br/copa2014/api/rest/empreendimento"
+}
 
 def browser():
 
-	address = path.join(CRAWLER_DIR,"phantomjs.exe")
-	return webdriver.PhantomJS(address)
+    address = path.join(CRAWLER_DIR,"phantomjs.exe")
+    return webdriver.PhantomJS(address)
 
 
 def save_csv(file=None,data=None):
-	if not file:
-		raise ValueError("File wasn't found")
-	if not data:
-		raise ValueError("The 'data' attribute should be filled")
+    if not file:
+        raise ValueError("File wasn't found")
+    if not data:
+        raise ValueError("The 'data' attribute should be filled")
 
-	file_path = path.join(CRAWLER_DIR,"data",file)
+    file_path = path.join(DATA_DIR,file)
 
-	data_columns = list(data[0].keys())
+    data_columns = list(data[0].keys())
 
-	with open(file_path,"w",newline="") as csv_file:
+    with open(file_path,"w",newline="") as csv_file:
 
-		csv_writer = csv.DictWriter(csv_file, fieldnames=data_columns,delimiter=";")
-		csv_writer.writeheader()
-		csv_writer.writerows(data)
+        csv_writer = csv.DictWriter(csv_file, fieldnames=data_columns,delimiter=";")
+        csv_writer.writeheader()
+        csv_writer.writerows(data)
 
-	print("The file {} was wrote successfully.".format(file))
+    print("The file {} was wrote successfully.".format(file))
+
+
+def load_xml_resource(filename):
+
+    file_path = path.join(DATA_DIR,filename)
+    
+    if not path.exists(file_path):
+        with open(file_path,"w") as xml:
+
+            url = URL_RESOURCES.get("world_cup")
+            
+            content = requests.get(url).text
+            xml.write(content)          
+    
+    with open(file_path) as xml:
+        root = ET.fromstring(xml.read())
+    return root
--- core/resources/crawlers/world_cup_expenses.py
+++ core/resources/crawlers/world_cup_expenses.py
@@ -1,13 +1,5 @@
-import xml.etree.ElementTree as ET
 from collections import OrderedDict,defaultdict
-from os import path , getcwd
-import requests
-from utils import save_csv
-from bs4 import BeautifulSoup
-
-URL = "http://www.portaltransparencia.gov.br/copa2014/api/rest/empreendimento"
-
-file_path = path.join(path.dirname(getcwd()),"data","world_cup_expense.xml")
+from utils import save_csv, load_xml_resource
 
 completed_spent_data = []
 ufs = {}
@@ -15,20 +7,20 @@ ufs = {}
 def calculate_executed_value(negotiated_value,percentual):
     return negotiated_value * percentual / 100
     
-def is_valid_file():
-    if path.exists(file_path):
-        if path.getsize(file_path) > 0:
-            return True
-        return False
-    return False
+def capitalize_text(text):
+    words = []
 
-if not is_valid_file():
-    with open(file_path,"w") as xml:
-        content = requests.get(URL).text
-        xml.write(content)
+    if len(text) > 1:
+        for part in text.split():
+            if len(part) > 3:
+                words.append(part.capitalize())
+            else:
+                words.append(part)
 
-with open(file_path) as xml:
-    root = ET.fromstring(xml.read())
+        return " ".join(words)
+    return text
+
+root = load_xml_resource("world_cup_expenses.xml")
 
 for children in root:
     uf = children.find("uf")
@@ -37,7 +29,7 @@ for children in root:
         _id = uf.find("id").text
         ufs[_id] = uf.find("nome").text
     
-for index,spent in enumerate(root):
+for spent in root:
     '''
      Fields to collect
 
@@ -87,15 +79,13 @@ for index,spent in enumerate(root):
 
         spent_data = OrderedDict()
         spent_data["uf"] = uf_name
-        spent_data["tema"] = theme
-        spent_data["tipo_instituicao"] = part_institution
-        spent_data["instituicao"] = name_institution
-        spent_data["status"] = status
+        spent_data["tema"] = capitalize_text(theme)
+        spent_data["tipo_instituicao"] = capitalize_text(part_institution)
+        spent_data["instituicao"] = capitalize_text(name_institution)
+        spent_data["status"] = capitalize_text(status)
         spent_data["negociado"] = negotiated
         spent_data["executado"] = calculate_executed_value(negotiated,executed)
 
         completed_spent_data.append(spent_data)     
-
-print([item for item in completed_spent_data if item["status"] != "Concluído"][0])
 
 save_csv("spent_world_cup.csv",data=completed_spent_data)
