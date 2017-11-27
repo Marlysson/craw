80ee560dbb6a2d902b8d8ebb1e3c04e14305c940
Marlysson <marlysson5@gmail.com>
2017-11-26 23:21:51 -0300

    Cleaning values from films and normalizing them multiplying them by one million.

--- core/resources/crawlers/films_money.py
+++ core/resources/crawlers/films_money.py
@@ -16,14 +16,24 @@ data = []
 for film_info in content_table.find_all("tr"):
 
 	name = film_info.find("td",class_="titleColumn").find("a").get_text()
-	bill_weekend = film_info.find("td",class_="ratingColumn").get_text()
-	gross_value = film_info.find("span",class_="secondaryInfo").get_text()
+	bill_weekend = film_info.find("td",class_="ratingColumn").get_text().replace("$","")
+
+	if bill_weekend.strip()[-1] == "M":
+		bill_weekend = float(bill_weekend.replace("M",""))
+		bill_weekend *= 1000000
+
+	gross_value = film_info.find("span",class_="secondaryInfo").get_text().replace("$","")
+
+	if gross_value.strip()[-1] == "M":
+		gross_value = float(gross_value.replace("M",""))
+		gross_value *= 1000000
+
 	week_rank = film_info.find("td",class_="weeksColumn").get_text()
 
 	item = OrderedDict()
 	item["nome"] = name.strip()
-	item["valor_semanal"] = bill_weekend.strip()
-	item["valor_acumulado"] = gross_value.strip()
+	item["valor_semanal"] = bill_weekend
+	item["valor_acumulado"] = gross_value
 	item["quantidade_semanas"] = week_rank.strip()
 
 	data.append(item)
